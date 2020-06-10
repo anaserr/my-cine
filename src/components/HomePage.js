fb1585f4c972ce6eec8e725f58fb658c09bd40ec
AnasErr <anaserrahmadi@gmail.com>
2020-06-10 12:04:21 +0200

    manage favoris & delete items

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -19,11 +19,18 @@ const HomePage = () => {
         }
     }, [movies$])
 
+    const deleteMovie = (e,id) => {
+        const res = window.confirm('Are you sure you wanna delete this movie ?')
+        const tab = [...movieList.filter(item => item.id  !== id)]
+
+        res && setMovieList(tab) 
+    }
+
     return (
         <div>
             {loading && <h3>loading...</h3>}
             <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', maxWidth: '1100px', margin: 'auto'}}>
-                {movieList.map(item => <MovieCard key={item.id} movie={item}/>)}
+                {movieList.map(item => <MovieCard key={item.id} movie={item} handleDelete={deleteMovie}/>)}
             </div>
         </div>
     );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import { faThumbsUp,faThumbsDown, faHeart as faHeartFull, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import { faThumbsUp,faThumbsDown, faHeart as faHeartFull, faTrashAlt, faHeartBroken } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({movie, handleDelete}) => {
     const likesWidth = (100 * movie.likes)/(movie.likes + movie.dislikes) ;
     const dislikesWidth = (100 * movie.dislikes)/(movie.likes + movie.dislikes) ;
+
+    const [liked, setLiked] = useState(null)
+
+    const handleFavorits = (e) => {
+
+        if(liked === true) {
+            movie.dislikes++
+            movie.likes > 0 && movie.likes--
+        } else {
+            movie.likes++
+            (movie.dislikes > 0 && liked !== null) && movie.dislikes--
+        }
+        setLiked(prevState =>  !prevState)
+        
+    }
+
     return (
         <div style={{background: 'linear-gradient(46deg, rgb(0, 0, 0), rgba(30, 30, 17, 0.8))',color:'#fff', border: '1px solid black',minWidth: '195px', height: '250px', margin: '5px 0', position: 'relative'}}>
-            <button style={{position: 'absolute', top: '2px', right: '2px',border: '0',outline: 'none', fontSize: '25px',background: 'none', color: '#eed522'}}>
-                <FontAwesomeIcon icon={faHeartFull} />
+            <button onClick={handleFavorits} style={{position: 'absolute', top: '2px', right: '2px',border: '0',outline: 'none', fontSize: '25px',background: 'none', color: '#eed522'}}>
+                <FontAwesomeIcon icon={liked === null ? faHeart : (liked===true) ? faHeartFull  : faHeartBroken} />
             </button>
             <div style={{position: 'absolute', top: '20px', left: '5px', textAlign: 'left'}}>
                 <div style={{color: '#fff', fontSize: '34px', marginBottom: '5px', fontWeight: 'bold', wordBreak: 'break-word'}}>{movie.title}</div>
                 <div style={{color: '#eed522',fontSize: '15px', fontStyle: 'italic'}}>{movie.category}</div>
             </div>
             
-            <button style={{position: 'absolute', bottom: '28px', left: '0px',border: '0',outline: 'none', fontSize: '16px',background: 'none', color: '#fff'}}>
-                    <FontAwesomeIcon icon={faTrashAlt} />
-                </button>
+            <button onClick={(e)=>handleDelete(e,movie.id)} id={movie.id} style={{position: 'absolute', bottom: '35px', left: '0px',border: '0',outline: 'none', fontSize: '16px',background: 'none', color: '#fff'}}>
+                <FontAwesomeIcon icon={faTrashAlt} />
+            </button>
 
             <div style={{position: 'absolute', bottom: '3px', left: '0px', textAlign: 'left', width: '100%',height: '5px', display: 'flex', color: '#fff'}}>
                 <div style={{background: '#bababa', width: likesWidth+'%'}}></div>
